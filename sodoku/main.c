b7329589267169efdf05cde3b17edfb8d848f1d4
Nathan Fusselman <44579712+nathanfusselman@users.noreply.github.com>
2020-12-19 19:03:42 -0600

    Initial Commit

---
+++ sodoku/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sodoku
+//
+//  Created by Nathan Fusselman on 12/19/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
